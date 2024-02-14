9e57ae906ad535f2793a25d1be550ed15d3bcff6
MarwaThabit <125121030+MarwaThabit@users.noreply.github.com>
2024-02-14 07:45:49 +0200

    Update Even, Odd, Positive and Negative.cpp

--- Even, Odd, Positive and Negative.cpp
+++ Even, Odd, Positive and Negative.cpp
@@ -1,20 +1,22 @@
-﻿// Even, Odd, Positive and Negative.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+int main() {
+  int n,N,countEven=0,countOdd=0,countPos=0,countNeg=0;
+  cin>>n;
+  for(int i=1;i<=n;i++){
+      cin>>N;
+     if(N%2==0){
+         countEven++;
+     }else{
+         countOdd++;
+     }
+     if(N>0){
+         countPos++;
+     }else if(N<0){
+         countNeg++;
+     }
+  }
+  cout<<"Even: "<<countEven<<endl<<"Odd: "<<countOdd<<endl<<"Positive: "<<countPos<<endl<<"Negative: "<<countNeg;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
