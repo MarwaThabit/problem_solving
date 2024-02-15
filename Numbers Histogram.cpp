2f35f8c4025573fe04f19878ec42f1023285b00f
MarwaThabit <125121030+MarwaThabit@users.noreply.github.com>
2024-02-15 06:41:40 +0200

    Numbers Histogram.cpp

---
+++ Numbers Histogram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char x;
+    cin>>x;
+    int n,N;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>N;
+        for(int j=1;j<=N;j++){
+            cout<<x;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
